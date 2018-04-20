1e42fce76cc3e3d9cd733dba111620b4601b418a
Victor Lellis <vmlellis@gmail.com>
2018-04-19 22:30:06 -0300

    Add space in comment

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, CategoryItem
 
 
-#Connect to Database and create database session
+# Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
